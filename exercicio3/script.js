f2fef38bc8523859ee5d89a874f6cde9cfe4537a
Clara <claradsfcarneiro@gmail.com>
2022-09-08 23:15:19 -0300

    Realizar exercício 3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,5 @@
-//código a ser reescrito
+const nome = prompt("Qual é o seu nome?");
+const idade = Number(prompt("Qual é a sua idade?"));
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
-
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+nome === "José" ? console.log("Oi, Zé!") : console.log("Olá, "+nome);
+idade >= 18 ? console.log("pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista");
